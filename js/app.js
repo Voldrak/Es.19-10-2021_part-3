60cce6bef9f9e72686f8467dfc5f28bd431e8e62
Marco Messina <voldrak94@gmail.com>
2021-10-19 17:32:03 +0200

    terzo esercizio completato con aggiunta di true

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 let myAnimal = prompt("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda");
 
-let chose = (myAnimal == 1) ? "Chose the Dog" 
-        : (myAnimal == 2) ? "Chose the Cat" 
-        : (myAnimal == 3) ? "Chose the Goldfish" 
-        : (myAnimal == 4) ? "Chose the Monkey" 
-        : (myAnimal == 5) ? "Chose the Panda" 
-        : "Animal not found"; 
+let chose = (myAnimal == 1 && true) ? "chose the Dog"
+        : (myAnimal == 2 && true) ? "chose the Cat"
+        : (myAnimal == 3 && true) ? "chose the Goldfish"
+        : (myAnimal == 4 && true) ? "chose the Monkey"
+        : (myAnimal == 5 && true) ? "chose the Panda"
+        : "Animal not found"
 console.log(chose)
